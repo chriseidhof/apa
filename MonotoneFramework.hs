e7ca57d005d03a06f8e75835a6417f966a4fb75e
rui <macbook@macbook-de-macbook.local>
2009-02-14 17:22:54 +0000

    monotone framework functions extended with new constructs

--- MonotoneFramework.hs
+++ MonotoneFramework.hs
@@ -12,12 +12,18 @@ flowR s = [(l, l') | (l', l) <- flow s]
 
 flow :: Stmt -> FlowGraph
 flow (Ass _ _ _)       = []
+flow (MultAss _ _)     = []
+flow (Print _ _)       = []
 flow (Skip _)          = []
+--flow (Continue _)
+--flow (Break _)
 flow (Seq s1 s2)       = unionL [flow s1, flow s2, [(l, init s2) | l <- final s1]]
 flow (While cond l s)  = unionL [flow s, [(l, init s)], [(l', l) | l' <- final s]]
 
 final :: Stmt -> [Label]
 final (Ass _ _ l)       = [l]
+final (MultAss _ l)     = [l]
+final (Print _ l)       = [l]
 final (Skip l)          = [l]
 final (Seq s1 s2)       = final s2
 final (While cond l s)  = [l]
@@ -25,6 +31,8 @@ final (While cond l s)  = [l]
 init :: Stmt -> Label
 init (Ass _ _ l)       = l
 init (Skip l)          = l
+init (MultAss _ l)     = l
+init (Print _ l)       = l
 init (Seq s1 s2)       = init s1
 init (While cond l s)  = l
 
