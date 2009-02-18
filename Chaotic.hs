0224180f6f6e1bacd27047c1a0023419504c2408
Chris Eidhof <ce@tupil.com>
2009-02-18 15:15:50 +0100

    iteration results

--- Chaotic.hs
+++ Chaotic.hs
@@ -3,6 +3,7 @@ module Chaotic where
 import Prelude hiding (init)
 import Data.Set ((\\))
 import Data.Maybe (fromJust)
+import Data.List (sortBy, transpose, intercalate)
 import qualified Data.Set as S
 import qualified Data.Map as M
 import Program
@@ -62,8 +63,45 @@ startIteration p iota = let vals = map (\l->((l,S.empty),(l,if(l`elem`final p) t
                             (env,exv) = unzip vals
                         in  (M.fromAscList env, M.fromAscList exv)
 
+data Cell = Cell {name :: String, lattice :: L}
+data Row  = Row String [L]
+
+formatTable :: [Row] -> String
+formatTable = intercalate " \\\\ \n" . map showRow
+
+showRow (Row s l) = s ++ " & " ++ lattices (map showLattice l)
+   where lattices = intercalate "&" . map latexSet
+         latexSet "" = "\\emptyset"
+         latexSet x  = "\\{" ++ x ++ "\\}"
+
+showLattice = intercalate "," . S.toList
+
+table p iota = let first   = startIteration p iota
+                   eqs     = equations p
+                   allIterations = takeWhileChanging (iterate (f eqs) first)
+                   toRow cells = if allEqual (map name cells)
+                                 then Row (name (head cells)) (map lattice cells)
+                                 else error "not all cells have been grouped correctly"
+               in map toRow $ transpose $ map formatIteration allIterations -- zip before after
+
+formatIteration :: IterationResult -> [Cell]
+formatIteration (entries, exits) = concat $ zipWith two (format "entry" entries) (format "exit" exits)
+ where sortMap    = sortBy (\x y -> compare (fst x) (fst y)) . M.toAscList
+       format s   = map (mkCell s) . sortMap
+       mkCell s (l,r) = Cell ("SLV_{" ++ s ++ "}(" ++ show l ++ ")") r
+       two a b    = [a, b]
+
+takeWhileChanging []                   = []
+takeWhileChanging (x:y:xs) | x == y    = [x,y]
+takeWhileChanging (x:y:ys) | otherwise = x:(takeWhileChanging (y:ys))
+
+allEqual []     = True
+allEqual (x:xs) = all (== x) xs
+
 -- TESTING
 p = labelProgram prog
 step = f (equations p)
 test r = do let l = labels p
             mapM_ (\lbl -> putStrLn $ (show lbl ++ ": " ++ (unwords $ S.toList $ slvEntry' lbl r) ++ " | " ++ (unwords $ S.toList $ slvExit' lbl r))) l
+
+test2 = putStrLn $ formatTable . table p $ S.singleton "r"
