ced1f6b1a75e58d2246d45242b591cf2545eb57a
rui <macbook@macbook-de-macbook.local>
2009-03-27 18:39:09 +0000

    consrtaint solving.

--- assignment3/Constraints.hs
+++ assignment3/Constraints.hs
@@ -2,7 +2,7 @@ module Constraints where
 import SemiLattice
 
 data Constraint a b = a :< b
-
+{--
 solve :: (Eq lat, SemiLattice lat) => [Int] -> [Constraint (Either Int lat) Int] -> [(Int, lat)]
 solve vars const = fixpoint const (map (\x->(x,bottom)) vars)
 
@@ -15,13 +15,13 @@ step const v = foldr stepSingle v const
 stepSingle :: (Eq lat, SemiLattice lat) => Constraint (Either Int lat) Int -> [(Int,lat)] -> [(Int,lat)]
 stepSingle (Left x  :< var) val = val `with` (var, (val $$ var) \/ (val $$ x))
 stepSingle (Right l :< var) val = val `with` (var, (val $$ var) \/ l)
+--}
+($$) :: (Eq a,SemiLattice lat) => [(a,lat)] -> a -> lat
+l $$ k = maybe bottom id (lookup k l)
 
-($$) :: (SemiLattice lat) => [(a,lat)] -> a -> lat
-($$) = (maybe id bottom) . flip lookup
-
-
-[] `with` (k,v) = [(k,v)]
-[(k',v'):kvs] `with` (k,v) 
+with :: Eq a => [(a,b)]->(a,b)->[(a,b)]
+[]          `with` (k,v) = [(k,v)]
+((k',v'):kvs) `with` (k,v) 
    | k == k'    = (k,v):kvs
    | otherwise  = (k',v'):(kvs `with` (k,v))
 
