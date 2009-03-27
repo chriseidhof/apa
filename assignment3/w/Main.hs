d26c90329fb773077b87807362b57b98700635b1
Chris Eidhof <ce@tupil.com>
2009-03-27 15:55:59 +0100

    Finished algorithm W

--- assignment3/w/Main.hs
+++ assignment3/w/Main.hs
@@ -11,7 +11,7 @@ data Ann = S | D | AVar Int
 type Var = Char
 type TVar = Int
 
-data Op = Plus | Minus
+data Op = Plus | GTE
 
 data Expr = CInt  Int 
           | CBool Bool
@@ -35,11 +35,41 @@ w gamma (Var   v) = return (lookup' v gamma, id)
 w gamma (Fn x e)  = do alpha <- fresh
                        (tau, theta) <- w (gamma @-> (x, alpha)) e
                        return (theta alpha --> tau, theta)
+w gamma (Fun f x e)  = do ax <- fresh
+                          a0 <- fresh
+                          (tau, theta0) <- w (gamma @-> (f, ax --> a0) @-> (x, ax)) e
+                          let theta1 = unify tau (theta0 a0)
+                          return (theta1 (theta0 ax) --> theta1 tau, theta1 . theta0)
 w gamma (App e1 e2) = do (tau1, theta1) <- w gamma e1
-                         (tau2, theta2) <- w (subst theta1 gamma) e2
+                         (tau2, theta2) <- w (theta1 <.> gamma) e2
                          alpha <- fresh
                          let theta3 = unify (theta2 tau1) (tau2 --> alpha)
                          return (theta3 alpha, theta3 . theta2 . theta1)
+w gamma (If e0 e1 e2) = do (t0, th0) <- w (                gamma) e0
+                           (t1, th1) <- w (        th0 <.> gamma) e1
+                           (t2, th2) <- w (th1 <.> th0 <.> gamma) e2
+                           let  th3  =  unify (th2 (th1 t0)) (Bool ())
+                                th4  =  unify (th3 t2) (th3 (th2 t1))
+                           return (th4 (th3 t2), th4 . th3 . th2 . th1 . th0)
+w gamma (Let x e1 e2) = do (t1, th1) <- w gamma e1
+                           (t2, th2) <- w ((th1 <.> gamma) @-> (x, t1))  e2
+                           return (t2, th2 . th1)
+w gamma (Op e1 op e2) = do (t1, th1) <- w gamma e1
+                           (t2, th2) <- w (th1 <.> gamma) e2
+                           let  th3  =  unify (th2 t1) (opTypeL op)
+                                th4  =  unify (th3 t2) (opTypeR op)
+                           return (opType op, th4 . th3 . th2 . th1)
+
+opTypeL = fst3 . opType'
+opTypeR = snd3 . opType'
+opType  = thd3 . opType'
+
+opType' Plus = (Int (), Int (), Int  ())
+opType' GTE  = (Int (), Int (), Bool ())
+
+fst3 (x,_,_) = x
+snd3 (_,x,_) = x
+thd3 (_,_,x) = x
 
 unify :: Type () -> Type () -> Subst ()
 unify (Int  _) (Int _)                        = id
@@ -48,16 +78,12 @@ unify (Function t1 t2 _) (Function t1' t2' _) = let theta1 = unify t1 t1'
                                                     theta2 = unify (theta1 t2) (theta1 t2')
                                                 in  theta2 . theta1
 unify (TVar a) (TVar b) | a == b = id
-unify t        (TVar a) | a `elem` (variables t) = error "Infinite type"
+unify t        (TVar a) | a `elem` (variables t) = error $ "Infinite type: " ++ show a ++ " occurs in " ++ show t
                         | otherwise = a |-> t
 unify (TVar a) t        = unify t (TVar a)
 unify t1       t2       = error $ "Can not unify" ++ show t1 ++ " and " ++ show t2
 
 lookup' x = maybe (error $ "No such variable: " ++ show x) id . lookup x
-
-(|->) :: TVar -> Type a -> Subst a
-a |-> t = replace a t
-
 
 gamma @-> x = x:gamma
 
@@ -69,13 +95,14 @@ variables (Function x y a)    = variables x ++ variables y
 variables _                   = []
 
 
-subst :: Subst a -> Context a -> Context a
-subst s gamma = map (\(v,t) -> (v, s t)) gamma
+infixr <.>
+(<.>) :: Subst a -> Context a -> Context a
+s <.> g = map (\(v,t) -> (v, s t)) g
 
-replace :: TVar -> Type a -> Type a -> Type a
-replace v t (TVar x) | v == x   = t
-replace v t (Function x y a)    = Function (replace v t x) (replace v t y) a
-replace _ _ x                   = x
+(|->) :: TVar -> Type a -> Subst a
+(|->) v t (TVar x) | v == x   = t
+(|->) v t (Function x y a)    = Function ((v |-> t) x) ((v |-> t) y) a
+(|->) _ _ x                   = x
 
 fresh :: W (Type a)
 fresh = do x <- get
