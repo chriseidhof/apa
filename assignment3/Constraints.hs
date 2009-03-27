da3c27d7b1e3c28c8d506b89bbcfa92506a8856d
rui <macbook@macbook-de-macbook.local>
2009-03-27 18:29:25 +0000

    almost: constraint solving

--- assignment3/Constraints.hs
+++ assignment3/Constraints.hs
@@ -1,25 +1,29 @@
 module Constraints where
+import SemiLattice
 
 data Constraint a b = a :< b
 
-solve :: (SemiLattice lat) => [Int] -> [Constraint (Either Int lat) Int] -> [(Int, lat)]
+solve :: (Eq lat, SemiLattice lat) => [Int] -> [Constraint (Either Int lat) Int] -> [(Int, lat)]
 solve vars const = fixpoint const (map (\x->(x,bottom)) vars)
 
-fixpoint :: (SemiLattice lat) => [Constraints (Either Int lat) Int] -> [(Int, lat)] -> [(Int, lat)]
+fixpoint :: (Eq lat, SemiLattice lat) => [Constraint (Either Int lat) Int] -> [(Int, lat)] -> [(Int, lat)]
 fixpoint c v = let v' = step c v in if (v==v') then v else fixpoint c v'
 
-step :: (SemiLattice lat) => [Constraints (Either Int lat) Int] -> [(Int, lat)] -> [(Int, lat)]
-step const = foldr  stepSingle const
+step :: (Eq lat, SemiLattice lat) => [Constraint (Either Int lat) Int] -> [(Int, lat)] -> [(Int, lat)]
+step const v = foldr stepSingle v const
 
-stepSingle :: (SemiLattice lat) => Constraint (Either Int lat) -> [(Int,lat)] -> [(Int,lat)]
-stepSingle (Left x  :< var) val = val `where` (var,val $$ var \/ val $$ x)
-stepSingle (Right l :< var) val = val `where` (var,val $$ var \/ l)
+stepSingle :: (Eq lat, SemiLattice lat) => Constraint (Either Int lat) Int -> [(Int,lat)] -> [(Int,lat)]
+stepSingle (Left x  :< var) val = val `with` (var, (val $$ var) \/ (val $$ x))
+stepSingle (Right l :< var) val = val `with` (var, (val $$ var) \/ l)
 
-($$) = flip lookup
+($$) :: (SemiLattice lat) => [(a,lat)] -> a -> lat
+($$) = (maybe id bottom) . flip lookup
 
 
-[] `where` (k,v) = [(k,v)]
-[(k',v'):kvs] `where` (k,v) 
+[] `with` (k,v) = [(k,v)]
+[(k',v'):kvs] `with` (k,v) 
    | k == k'    = (k,v):kvs
-   | otherwies  = (k',v'):(kvs `where` (k,v))
+   | otherwise  = (k',v'):(kvs `with` (k,v))
+
+
 
